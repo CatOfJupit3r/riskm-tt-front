6871a441f231451605d10eec7164fe0035bc4f14
CatOfJupit3r <catofjupit3r@gmail.com>
2024-09-13 20:28:52 +0300

    refactor: extract header to separate component, `<Header />`. add lazy loading for header.

---
+++ src/components/header.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Button } from '@components/ui/button'
+import { useUserContext } from '@context/UserContext'
+
+const Header = () => {
+    const { username, changeUsername } = useUserContext()
+
+    return <header className={'flex w-screen flex-row items-center justify-between bg-gray-800 p-4 text-white'}>
+        <img
+            src={'https://cdn.discordapp.com/attachments/1035558785051275265/1284198502565417041/1284198376111472690.webp?ex=66e5c28e&is=66e4710e&hm=63c2712b932f4354313cf505a5e003031b6e3324715bec0eb703e1a9e06da45f&'}
+            alt={'logo'}
+            className={'size-10'}
+        />
+        {
+            username !== null && <div className={'flex flex-row items-center gap-4'}>
+                <p className={'text-lg'}>
+                    @{username}
+                </p>
+                <Button
+                    variant={'ghost'}
+                    className={'text-lg'}
+                    onClick={() => {
+                        window.localStorage.removeItem('username')
+                        changeUsername(null)
+                    }}
+                >
+                    Sign out
+                </Button>
+            </div>
+        }
+    </header>
+}
+
+export default Header
--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,39 +1,19 @@
 import { useUserContext } from '@context/UserContext'
 import ContentPage from '@pages/ContentPage'
 import AuthPage from '@pages/AuthPage'
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Toaster } from '@components/ui/toaster'
-import { Button } from '@components/ui/button'
+
+const Header = lazy(() => import('@components/header'))
 
 const Router = () => {
-    const { username, changeUsername } = useUserContext()
+    const { username } = useUserContext()
 
     return <div>
         <Toaster />
-        <header className={'flex w-screen flex-row items-center justify-between bg-gray-800 p-4 text-white'}>
-            <img
-                src={'https://cdn.discordapp.com/attachments/1035558785051275265/1284198502565417041/1284198376111472690.webp?ex=66e5c28e&is=66e4710e&hm=63c2712b932f4354313cf505a5e003031b6e3324715bec0eb703e1a9e06da45f&'}
-                alt={'logo'}
-                className={'size-10'}
-            />
-            {
-                username !== null && <div className={'flex flex-row items-center gap-4'}>
-                    <p className={'text-lg'}>
-                        @{username}
-                    </p>
-                    <Button
-                        variant={'ghost'}
-                        className={'text-lg'}
-                        onClick={() => {
-                            window.localStorage.removeItem('username')
-                            changeUsername(null)
-                        }}
-                    >
-                        Sign out
-                    </Button>
-                </div>
-            }
-        </header>
+        <Suspense fallback={<div className={'flex w-screen bg-gray-800 p-9'}/>}>
+            <Header />
+        </Suspense>
         <main className={'w-screen'}>
             {
                 username === null ? <AuthPage /> : <ContentPage />
