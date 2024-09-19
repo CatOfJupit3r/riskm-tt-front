e7083b89e78ace2803cdde613cefb0b0e0370e70
CatOfJupit3r <catofjupit3r@gmail.com>
2024-09-19 19:49:13 +0300

    chore: add log of backend url for testing purposes.

--- src/services/APIService.ts
+++ src/services/APIService.ts
@@ -14,6 +14,7 @@ class APIService {
     public client: ApolloClient<unknown>
 
     constructor() {
+        console.log('process.env.REACT_APP_BACKEND_URL', process.env.REACT_APP_BACKEND_URL)
         this.client = new ApolloClient({
             uri: process.env.REACT_APP_BACKEND_URL,
             cache: new InMemoryCache({}),
